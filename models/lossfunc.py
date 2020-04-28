9c276e28b8e791b5bd48c9184782df40152ca7c8
Tejvir Jogani <tjogani@berkeley.edu>
2020-04-29 00:36:18 +0530

    edits

--- models/lossfunc.py
+++ models/lossfunc.py
@@ -5,7 +5,8 @@ from torch.autograd import Variable
 class PerceptualLoss(nn.Module):
 
     """
-    Loss network is like VGG relu2_2
+    Loss network is like VGG relu2_2. The PerceptualLoss is one
+    that uses target features rather than per pixel losses.
     """
 
     def __init__(self, loss_network):
@@ -15,4 +16,5 @@ class PerceptualLoss(nn.Module):
     def forward(self, input, target):
         input_feature = self.loss_network(input)
         target_feature = self.loss_network(target)
+        #comparing target feautures of a loss network and our input
         return torch.mean((input_feature - target_feature) ** 2)
