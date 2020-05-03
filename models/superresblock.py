c48ec32d241946964c84fffad53935ab5a9bea7c
Nabeel Hingun <nabeel126@berkeley.edu>
2020-05-03 20:25:28 +0400

    debugging

--- models/superresblock.py
+++ models/superresblock.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from upsamplingblock import UpBlock
-from basicresnet import ResBlock
-from prelu import ConvPrelu
-from batchrelu import ConvBatchRelu
+from models.upsamplingblock import UpBlock
+from models.basicresnet import ResBlock
+from models.prelu import ConvPrelu
+from models.batchrelu import ConvBatchRelu
 
 class SuperRes(nn.Module):
     def __init__(self):
@@ -15,6 +15,8 @@ class SuperRes(nn.Module):
         self.upblock = UpBlock(64, 64, kernel_size=3, padding=1)
         self.conv_out = nn.Conv2d(64, 3, kernel_size=9, padding=4)
 #compiles all the blocks.
+    
+
     def forward(self, x):
         x = self.batch_norm(x)
         x = self.conv_in(x)
